5c6131714a6ca282a82b28bd668589efe0d6c5a7
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-17 13:10:05 +0700

    Fixture dengan Anotation @before

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -12,25 +12,34 @@ class PersonTest extends TestCase
     // Agar tidak membuat object person berulang-ulang.
     protected function setUp(): void
     {
+        // $this->person = new Person('Adi');
+    }
+
+    // CreatePerson() menggantikan setUp() diatas untuk membuat object person
+    /**
+     * @before
+     */
+    public function CreatePerson()
+    {
         $this->person = new Person('Adi');
     }
 
     public function testSuccess()
     {
-//        $person = new Person('Adi');
+       // $person = new Person('Adi');
         self::assertEquals("Hi Budi, my name is Adi", $this->person->sayHello('Budi'));
     }
 
     public function testException()
     {
-//        $person = new Person('Adi');
+        // $person = new Person('Adi');
         $this->expectException(\Exception::class);
         $this->person->sayHello(null);
     }
 
     public function testOutput()
     {
-//        $person = new Person('Adi');
+        // $person = new Person('Adi');
         $this->expectOutputString("Good bye Budi".PHP_EOL);
         $this->person->sayGoodBye('Budi');
     }
