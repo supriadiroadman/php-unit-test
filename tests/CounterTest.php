1d3a8d58d948d0dfa1ac3aaa4e898ac31620eb03
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-17 11:09:02 +0700

    Test Dependency

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -34,6 +34,26 @@ class CounterTest extends TestCase
         $counter->increment();
         Assert::assertEquals(1, $counter->getCounter());
     }
+
+    public function testFirst(): Counter
+    {
+        $counter = new Counter();
+        $counter->increment();
+        $this->assertEquals(1,$counter->getCounter());
+
+        return $counter;
+    }
+
+    /**
+     * @depends testFirst
+     */
+    public function testSecond(Counter $counter): void
+    {
+        $counter->increment();
+        $this->assertEquals(2,$counter->getCounter());
+    }
+
+
 }
 
 
