c2627805f0eef4d835f0a9466d970229a2875fad
Supriadi Roadman <supriadiroadman@gmail.com>
2022-05-17 11:21:24 +0700

    Test Manual tanpa 'Data Provider'

---
+++ tests/MathTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Supriadi\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase {
+
+    public function testManual()
+    {
+        self::assertEquals(10, Math::sum([5,5]));
+        self::assertEquals(20, Math::sum([4,4,4,4,4]));
+        self::assertEquals(9, Math::sum([3,3,3]));
+        self::assertEquals(0, Math::sum([]));
+        self::assertEquals(2, Math::sum([2]));
+    }
+
+}
